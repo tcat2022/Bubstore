d86c9ac1846216fc9bf07fd55f0dd20d5ae5bf15
tommy <tcatty7@gmail.com>
2023-11-15 14:02:33 -0500

    bubba pd website is finished
    ps......
    
    im not a winkie

---
+++ script.js
@@ -0,0 +1,6 @@
+let search = document.getElementById("button");
+let searchField = document.getElementById("search");
+
+search.addEventListener("click", () => {
+    alert("Bubba PD is watching")
+})
